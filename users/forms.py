1bec0ccac6f10ed0f0b5bf2d49e0651323bf3af4
texaskyle <evansnjoroge3345@gmail.com>
2023-10-22 22:03:13 +0300

    register users using forms

---
+++ users/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, help_text='Required.')
+    last_name = forms.CharField(max_length=30, help_text='Required.')
+    email = forms.EmailField()
+
+    class Meta:
+         model = User
+         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
