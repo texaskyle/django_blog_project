338e715d89f649b3098361f738bae6eb4e99efbe
texaskyle <evansnjoroge3345@gmail.com>
2023-10-25 23:02:43 +0300

    class based view of post list, to display the posts in the home page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from blog.views import PostListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    # path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('messages/', views.messages, name='blog-messages'),
     path('article/', views.article, name="blog-article")
